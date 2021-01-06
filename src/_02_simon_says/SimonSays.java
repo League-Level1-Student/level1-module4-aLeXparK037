14a655056763a71eb42aa6e1989abf47fb5f9091
aLeXparK037 <65091873+aLeXparK037@users.noreply.github.com>
2021-01-05 18:30:20 -0800

    1212312

--- src/_02_simon_says/SimonSays.java
+++ src/_02_simon_says/SimonSays.java
@@ -30,30 +30,46 @@ public class SimonSays extends KeyAdapter {
 
 	// Complete steps 1 - 7 before you test
 	// 1. Declare a JFrame variable
-
+JFrame frame = new JFrame();
 	public void run() {
 		// 2. Add the four images that match keyboard keys like this:
 		// images.put(KeyEvent.VK_UP, "up.jpg");
+		images.put(KeyEvent.VK_UP, "up.jpg");
+		images.put(KeyEvent.VK_DOWN, "down.jpg");
+		images.put(KeyEvent.VK_LEFT, "left.jpg");
+		images.put(KeyEvent.VK_RIGHT, "right.jpg");
 
 		// 3. Use a JOptionPane to tell the user the rules: "Press the matching
 		// key when
 		// 'Simon says' otherwise press a different key"
+		JOptionPane.showMessageDialog(null, "Press the matching key when, 'simon says', otherwise press a different key.");
 
 		// 4. Call the showImage method to show an image
+		showImage();
 
 	}
 
 	public void keyPressed(KeyEvent e) {
 		// 15. Make a points variable to track the score.
+		int points = 0;
 
 		// 16. If the keyCode matches the imageIndex and "Simon says"
+		
 
 		// 17. Increase the value of score
+		if(e.getKeyCode()==(imageIndex)  && (simonSays)) {
+			points +=1;
+			speak("Correct");
+		}
 
 		// 18. Use the speak method to tell the user they were correct
+		
 
 		// 19. If the keyCode doesn't match the imageIndex and "Simon didn't
 		// say..."
+		if(e.getKeyCode()!= (imageIndex) && (simonSays)) {
+			
+		}
 
 		// 20. Increase the value of score
 
