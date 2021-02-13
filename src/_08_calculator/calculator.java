2273e68c168abcf02a34226398c6df08e092c5bf
aLeXparK037 <65091873+aLeXparK037@users.noreply.github.com>
2021-02-13 12:57:18 -0800

    12312312

--- src/_08_calculator/calculator.java
+++ src/_08_calculator/calculator.java
@@ -1,5 +1,82 @@
 package _08_calculator;
 
-public class calculator {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class calculator implements ActionListener {
+	JTextField fieldone = new JTextField(5);
+	JTextField fieldtwo = new JTextField(5);
+	
+	JButton one = new JButton();
+	JButton two = new JButton();
+	JButton three = new JButton();
+	JButton four = new JButton();
+	JFrame frame = new JFrame();
+
+	JLabel label = new JLabel();
+
+	
+	public void calculatorbuttons() {
+		frame.setVisible(true);
+		JPanel panel = new JPanel();
+		frame.add(panel);
+		
+		panel.add(fieldone);
+		panel.add(fieldtwo);
+		
+		panel.add(label);
+	
+		
+
+		
+		one.addActionListener(this);
+		panel.add(one);
+		one.setText("Add");
+		panel.add(two);
+		two.setText("sub");
+		two.addActionListener(this);
+		panel.add(three);
+		three.setText("div");
+		three.addActionListener(this);
+		panel.add(four);
+		four.addActionListener(this);
+		four.setText("mult");
+
+		frame.pack();
+	}
+	
+public void add(int a, int b) {
+	int answer = a + b;
+	 label.setText(answer + "");
+	 frame.pack();
+	}
+public void sub() {
+	
+	}
+public void div() {
+	
+}
+public void mult() {
+	
+}
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	if (e.getSource()== one) {
+		String response = fieldone.getText();
+		int a = Integer.parseInt(response);
+		String responses = fieldtwo.getText();
+		int b = Integer.parseInt(responses);
+	add(a,b);	
+	
+	}
+}
 
 }
--- src/_08_calculator/calculatorrunnner.java
+++ src/_08_calculator/calculatorrunnner.java
@@ -1,5 +1,7 @@
 package _08_calculator;
 
 public class calculatorrunnner {
-
+public static void main(String[] args) {
+	new calculator().calculatorbuttons();
+}
 }
