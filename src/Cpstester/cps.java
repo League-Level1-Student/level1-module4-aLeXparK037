4127c41dd18ede89e7286770785c0041bf8033f9
aLeXparK037 <65091873+aLeXparK037@users.noreply.github.com>
2021-03-09 10:55:34 -0800

    123123123123

--- src/Cpstester/cps.java
+++ src/Cpstester/cps.java
@@ -2,21 +2,21 @@ package Cpstester;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Date;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class cps implements ActionListener {
 
 	JButton button = new JButton();
-	int cpstrack;
-	Date one;
+	int cpstrack = 0;
+	Timer time = new Timer(1000, null);
+	//Date one;
 
 	public void run() {
-		one = new Date();
+		
 		JFrame frame = new JFrame();
 		frame.setVisible(true);
 		JPanel panel = new JPanel();
@@ -32,19 +32,26 @@ public class cps implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		
 		if (e.getSource()==button) {
-			
-			cpstrack +=1;
-			if (cpstrack==10) {
-				endGame(one,cpstrack);
-				cpstrack = 0;
+			if (cpstrack == 0) {
+				time.start();
+				cpstrack +=1;
+			} else {
+				cpstrack +=1;
 			}
+			
+			
+		}
+	
+		if (time.isRunning()==false) {
+			JOptionPane.showMessageDialog(null, "you clicked " + cpstrack + " clicks in one second");
 		}
 	}
-	private void endGame(Date timeAtStart, int molesWhacked) { 
+	/*private void endGame(Date timeAtStart, int molesWhacked) { 
 	    Date timeAtEnd = new Date();
 	    JOptionPane.showMessageDialog(null, "Your click rate is "
 	            + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 60.00 / molesWhacked)
 	                  + " clicks per second.");
-	}
-}
+	*/}
+
