c53a70e7cf8d97a1ff855be0f4bff895132e30c1
aLeXparK037 <65091873+aLeXparK037@users.noreply.github.com>
2021-03-09 09:49:36 -0800

    123123123

--- src/Cpstester/cps.java
+++ src/Cpstester/cps.java
@@ -1,22 +1,50 @@
 package Cpstester;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class cps {
+public class cps implements ActionListener {
 
-	
+	JButton button = new JButton();
+	int cpstrack;
+	Date one;
+
 	public void run() {
+		one = new Date();
 		JFrame frame = new JFrame();
 		frame.setVisible(true);
 		JPanel panel = new JPanel();
-		JButton button = new JButton();
 		
 		frame.add(panel);
 		panel.add(button);
 		button.setText("Click to begin");
+		button.addActionListener(this);
 		
 		frame.pack();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource()==button) {
+			
+			cpstrack +=1;
+			if (cpstrack==10) {
+				endGame(one,cpstrack);
+				cpstrack = 0;
+			}
+		}
+	}
+	private void endGame(Date timeAtStart, int molesWhacked) { 
+	    Date timeAtEnd = new Date();
+	    JOptionPane.showMessageDialog(null, "Your click rate is "
+	            + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 60.00 / molesWhacked)
+	                  + " clicks per second.");
 	}
 }
