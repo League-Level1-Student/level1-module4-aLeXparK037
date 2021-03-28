c809bfc41f588b353323b7fb2bc95923a8a90cfc
aLeXparK037 <65091873+aLeXparK037@users.noreply.github.com>
2021-03-27 17:42:19 -0700

    123123123123123123123

--- src/_12_slot_machine/slotmachine.java
+++ src/_12_slot_machine/slotmachine.java
@@ -11,6 +11,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class slotmachine implements ActionListener {
@@ -21,71 +22,89 @@ public class slotmachine implements ActionListener {
 	JLabel one;
 	JLabel two;
 	JLabel three;
-	JFrame frame = new JFrame();
-	JPanel panel = new JPanel();
-
+	JFrame frame;
+	JPanel panel;
+	int i;
+	int b;
 
 	public void run() {
+		frame = new JFrame();
+		panel = new JPanel();
 		frame.setVisible(true);
 		frame.add(panel);
 		panel.add(button);
+		populateSlotMachine();
 		button.setText("Click to spin!");
 		button.addActionListener(this);
 		frame.pack();
 	}
-	private JLabel createLabelImage(String fileName) throws MalformedURLException{
-        URL imageURL = getClass().getResource(fileName);
-	if (imageURL == null){
-		System.err.println("Could not find image " + fileName);
-		return new JLabel();
+
+	private void populateSlotMachine() {
+		for (i = 0; i < 3; i++) {
+			Random ran = new Random();
+			int num = ran.nextInt(3);
+
+			if (num == 0) {
+				try {
+					one = createLabelImage(apple);
+					panel.add(one);
+					frame.pack();
+					int b =+ 1;
+					if (b==3) {
+						JOptionPane.showMessageDialog(null, "You won");
+					}
+				} catch (MalformedURLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+
+				}
+
+			}
+			if (num == 1) {
+				try {
+					two = createLabelImage(orange);
+					panel.add(two);
+					frame.pack();
+					
+				} catch (MalformedURLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+			if (num == 2) {
+				try {
+					three = createLabelImage(cherry);
+					panel.add(three);
+					frame.pack();
+
+				} catch (MalformedURLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+
+				}
+			}
+		}
 	}
-	Icon icon = new ImageIcon(imageURL);
-	JLabel imageLabel = new JLabel(icon);
-	return imageLabel;
-}
+
+	private JLabel createLabelImage(String fileName) throws MalformedURLException {
+		URL imageURL = getClass().getResource(fileName);
+		if (imageURL == null) {
+			System.err.println("Could not find image " + fileName);
+			return new JLabel();
+		}
+		Icon icon = new ImageIcon(imageURL);
+		JLabel imageLabel = new JLabel(icon);
+		return imageLabel;
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if (e.getSource()==button) {
-			
-			for (int i = 1; i < 4; i++) {
-				Random ran = new Random();
-				int num = ran.nextInt(4);
-				
-				if (num == 1) {
-					try {
-						one = createLabelImage(apple);
-						panel.add(one);
-						frame.pack();
-					} catch (MalformedURLException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-						
-					}
-					
-				} 
-				if (num == 2) {
-					try {
-						two = createLabelImage(orange);
-						panel.add(two);
-						frame.pack();
-					} catch (MalformedURLException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-				} 
-				if (num == 3) {
-					try {
-						three = createLabelImage(cherry);
-						panel.add(three);
-						frame.pack();
-					} catch (MalformedURLException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
+		if (e.getSource() == button) {
+			panel.removeAll();
+			panel.add(button);
+			populateSlotMachine();
 
-					}
-				}
-			}
 		}
 	}
 }
